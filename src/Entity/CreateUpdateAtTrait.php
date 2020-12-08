8e0f7424cad346ef4745b7ded412ed6173051c7c
evrinoma <evrinoma@gmail.com>
2020-12-08 20:44:00 +0300

    fix createUpdate

--- src/Entity/CreateUpdateAtTrait.php
+++ src/Entity/CreateUpdateAtTrait.php
@@ -3,6 +3,9 @@
 
 namespace Evrinoma\UtilsBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation\Type;
+
 /**
  * Trait CreateUpdateTrait
  *
@@ -39,5 +42,29 @@ trait CreateUpdateAtTrait
     public function getUpdatedAt(): \DateTime
     {
         return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     *
+     * @return $this
+     */
+    public function setCreatedAt(\DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     *
+     * @return $this
+     */
+    public function setUpdatedAt(\DateTime $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
     }
 }
