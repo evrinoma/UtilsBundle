855301d1e9955b072f2baad6a4d5767fc56a4bc8
evrinoma <evrinoma@gmail.com>
2021-02-23 19:27:06 +0300

    fix lexik dependency
    Signed-off-by: evrinoma <evrinoma@gmail.com>

--- src/DependencyInjection/EvrinomaUtilsExtension.php
+++ src/DependencyInjection/EvrinomaUtilsExtension.php
@@ -5,7 +5,9 @@ namespace Evrinoma\UtilsBundle\DependencyInjection;
 
 use Evrinoma\UtilsBundle\EvrinomaUtilsBundle;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
@@ -22,13 +24,51 @@ class EvrinomaUtilsExtension extends Extension
         $configuration   = $this->getConfiguration($configs, $container);
         $config          = $this->processConfiguration($configuration, $configs);
         $this->container = $container;
-        $ldapServers        = $config['ldap_servers'];
+        $ldapServers     = $config['ldap_servers'];
         if ($ldapServers) {
             $definition = $this->container->getDefinition('evrinoma.security.provider.ldap');
             $definition->addMethodCall('setServers', [$ldapServers]);
         }
+
+        if (array_key_exists('LexikJWTAuthenticationBundle', $container->getParameterBag()->get('kernel.bundles'))) {
+            $this->addDefinition(
+                'Evrinoma\UtilsBundle\Security\Guard\JWT\AuthenticatorGuard',
+                'evrinoma.security.guard.jwt',
+                [new Reference('Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface')],
+                true
+            );
+        }
     }
 //endregion Public
+
+//region SECTION: Private
+    /**
+     * @param string $className
+     * @param string $aliasName
+     * @param        $arguments
+     * @param false  $public
+     *
+     * @return Definition
+     */
+    private function addDefinition(string $className, string $aliasName, $arguments, $public = false): Definition
+    {
+        $definition = new Definition($className);
+        $alias      = new Alias($aliasName);
+
+        if ($public) {
+            $definition->setPublic(true);
+            $alias->setPublic(true);
+        }
+        $this->container->addDefinitions([$aliasName => $definition]);
+        $this->container->addAliases([$className => $alias]);
+
+        foreach ($arguments as $key => $argument) {
+            $definition->setArgument($key, $argument);
+        }
+
+        return $definition;
+    }
+//endregion Private
 
 //region SECTION: Getters/Setters
     public function getAlias()
