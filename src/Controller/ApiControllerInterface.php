9e037283d6ffaf62a0df59d62a3422401c5b48ef
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-22 09:41:21 +0300

    refactoring api controller as ddd approach

---
+++ src/Controller/ApiControllerInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Evrinoma\UtilsBundle\Controller;
+
+use Evrinoma\UtilsBundle\Rest\RestInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+interface ApiControllerInterface
+{
+//region SECTION: Public
+    public function putAction(): JsonResponse;
+
+    public function postAction(): JsonResponse;
+
+    public function deleteAction(): JsonResponse;
+
+    public function criteriaAction(): JsonResponse;
+//endregion Public
+
+//region SECTION: Private
+//endregion Private
+
+//region SECTION: Getters/Setters
+    public function getAction(): JsonResponse;
+
+    public function setRestStatus(RestInterface $manager, \Exception $e): array;
+//endregion Getters/Setters
+}
