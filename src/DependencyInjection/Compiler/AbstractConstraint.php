3686578820b985f80618d596afdced87ececf166
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-23 17:06:20 +0300

    Compiler Pass logic

---
+++ src/DependencyInjection/Compiler/AbstractConstraint.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Evrinoma\UtilsBundle\DependencyInjection\Compiler;
+
+use Evrinoma\UtilsBundle\Exception\ConstraintCannotBeCompiledException;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+
+abstract class AbstractConstraint
+{
+    protected static string $alias = '';
+    protected static string $class = '';
+    protected static string $methodCall = '';
+
+    /**
+     * @inheritDoc
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (static::$alias === '' || static::$class ==='' || static::$methodCall ==='') {
+            throw new ConstraintCannotBeCompiledException();
+        }
+
+        if (!$container->has(static::$class)) {
+            return;
+        }
+
+        $definition = $container->findDefinition(static::$class);
+
+        $taggedServices = $container->findTaggedServiceIds(static::$alias);
+
+        foreach ($taggedServices as $id => $tags) {
+            $definition->addMethodCall(static::$methodCall, [new Reference($id)]);
+        }
+    }
+}
