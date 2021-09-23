2a716ed15296e3fbb42036acfcfe8f8ae6a0e719
Nikolay Nikolaev <evrinoma@gmail.com>
2021-09-23 17:01:54 +0300

    add DI Exception

---
+++ src/Exception/ConstraintCannotBeCompiledException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Evrinoma\UtilsBundle\Exception;
+
+class ConstraintCannotBeCompiledException extends \Exception
+{
+
+}
---
+++ src/Exception/MapEntityCannotBeCompiledException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Evrinoma\UtilsBundle\Exception;
+
+class MapEntityCannotBeCompiledException extends \Exception
+{
+
+}
