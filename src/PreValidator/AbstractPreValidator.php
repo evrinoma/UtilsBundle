a1ab5b62b0478b57d05114366363b701dbbc3b54
Nikolay Nikolaev <evrinoma@gmail.com>
2022-05-29 08:43:13 +0200

    little refactoring

--- src/PreValidator/AbstractPreValidator.php
+++ src/PreValidator/AbstractPreValidator.php
@@ -6,9 +6,9 @@ use Evrinoma\DtoBundle\Dto\DtoInterface;
 
 abstract class AbstractPreValidator
 {
-    abstract protected function onUpdate(DtoInterface $dto);
+    abstract protected function onPost(DtoInterface $dto);
 
-    abstract protected function onDelete(DtoInterface $dto): void;
+    abstract protected function onPut(DtoInterface $dto): void;
 
-    abstract protected function onCreate(DtoInterface $dto);
+    abstract protected function onDelete(DtoInterface $dto);
 }
