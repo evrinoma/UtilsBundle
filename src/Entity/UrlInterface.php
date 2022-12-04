12819528a1b9fd7e80f17c468082a049e71a77ea
Nikolay Nikolaev <evrinoma@gmail.com>
2022-12-04 23:02:20 +0300

    added new url entity

---
+++ src/Entity/UrlInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package.
+ *
+ * (c) Nikolay Nikolaev <evrinoma@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Evrinoma\UtilsBundle\Entity;
+
+interface UrlInterface
+{
+    /**
+     * @return string
+     */
+    public function getUrl(): string;
+
+    /**
+     * @param string $url
+     *
+     * @return self
+     */
+    public function setUrl(string $url): self;
+}
---
+++ src/Entity/UrlTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package.
+ *
+ * (c) Nikolay Nikolaev <evrinoma@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Evrinoma\UtilsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait UrlTrait
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255, nullable=false)
+     */
+    protected string $url;
+
+    /**
+     * @return string
+     */
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return self
+     */
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
