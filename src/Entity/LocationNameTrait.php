4bfd70c08832b3600c00bf1e0c41b7d58dae8c64
Nikolay Nikolaev <evrinoma@gmail.com>
2023-06-29 20:45:26 +0300

    - added new value object

---
+++ src/Entity/LocationNameTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package.
+ *
+ * (c) Nikolay Nikolaev <evrinoma@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Evrinoma\UtilsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait LocationNameTrait
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="location_name", type="string", length=255, nullable=false)
+     */
+    protected $locationName;
+
+    /*
+     * @return string
+     */
+    public function getLocationName(): ?string
+    {
+        return $this->locationName;
+    }
+
+    /**
+     * @param string $locationName
+     *
+     * @return self
+     */
+    public function setLocationName(string $locationName): self
+    {
+        $this->locationName = $locationName;
+
+        return $this;
+    }
+}
