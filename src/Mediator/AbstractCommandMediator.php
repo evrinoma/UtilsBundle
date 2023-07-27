b5e93f77986b41c4bb3d81f49c3447979c727a15
Nikolay Nikolaev <evrinoma@gmail.com>
2023-07-27 19:29:05 +0300

    - abstract layer was extended

--- src/Mediator/AbstractCommandMediator.php
+++ src/Mediator/AbstractCommandMediator.php
@@ -19,7 +19,7 @@ abstract class AbstractCommandMediator
 {
     abstract protected function onUpdate(DtoInterface $dto, $entity);
 
-    abstract protected function onDelete(DtoInterface $dto, $entity): void;
+    abstract protected function onDelete(DtoInterface $dto, $entity);
 
     abstract protected function onCreate(DtoInterface $dto, $entity);
 }
